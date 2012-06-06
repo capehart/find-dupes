2c4ef17018b45602c92b20a0925d0c1f47999f37
AC Capehart <github@accapehart.com>
2012-06-06 14:28:36 -0400

    Fixed issue where I'd refactored the md5file to use it as both main and library so I could use it as a library, but in doing so, I'd broken main.  This fixes main.

--- md5file.py
+++ md5file.py
@@ -46,7 +46,7 @@ def main(argv=None):
     parser = argparse.ArgumentParser(description='Get the MD5 sum of a file.')
     parser.add_argument('file', metavar='/path/to/file', help='The root level directory under which you\'d like to find duplicates.', default=None)
     args = parser.parse_args()
-    md5sum(args.file)
+    print md5sum(args.file)
 
 if __name__ == '__main__':
-    sys.exit(md5sum())
+    sys.exit(main())
